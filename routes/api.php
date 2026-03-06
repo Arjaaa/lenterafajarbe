3ffc0d539460ec3db8f22371e665b915f05c7bd1
Arjaaa <dhitoarza@gmail.com>
2026-03-06 20:26:08 +0700

    First commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RegisterController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[RegisterController::class,'register']);
